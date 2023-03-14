4c0a952b8f27d19ef4bae446f8f33e52da4bd1ea
on3gin <t22092006@gmail.com>
2023-03-14 17:38:27 +0300

    const add

---
+++ const.py
@@ -0,0 +1,7 @@
+GEOCODER_API = '40d1649f-0493-4b70-98ba-98533de7710b'
+ORG_API = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+
+GEOCODER_API_SERVER = "http://geocode-maps.yandex.ru/1.x/"
+SEARCH_API_SERVER = "https://search-maps.yandex.ru/v1/"
+MAP_API_SERVER = "http://static-maps.yandex.ru/1.x/"
+
