2e0bdd0063b3ed04b5732be22a3a6b18f0a14bf7
on3gin <t22092006@gmail.com>
2023-03-14 19:15:54 +0300

    version 1.0

--- const.py
+++ const.py
@@ -3,5 +3,5 @@ ORG_API = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
 
 GEOCODER_API_SERVER = "http://geocode-maps.yandex.ru/1.x/"
 SEARCH_API_SERVER = "https://search-maps.yandex.ru/v1/"
-MAP_API_SERVER = "http://static-maps.yandex.ru/1.x/"
+STATIC_API_SERVER = "http://static-maps.yandex.ru/1.x/"
 
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 #-*- coding: utf-8 -*-
 
+import os
+import const
+from params import params_func
 from ya_maps import Ui_ya_maps
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
+import requests
+import shutil
+
+
+POS = ['52.29723', '54.901171']
+DELTA = '0.01'
+SPN = 'map'
 
 
 class MainFunc(QMainWindow, Ui_ya_maps):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        if not os.path.exists('files//img'):
+            os.mkdir('files//img')
+
+        self.static_func()
+
+    def static_func(self):
+        params = params_func(POS, DELTA, SPN)
+        response = requests.get(const.STATIC_API_SERVER, params)
+
+        static_name = 'almet'
+        with open('files//img//' + static_name, "wb") as file:
+            file.write(response.content)
+
+        self.map.setPixmap(QPixmap('files//img//' + static_name))
+
+    def closeEvent(self, event):
+        if os.path.exists('files//img'):
+            shutil.rmtree('files//img')
 
 
 if __name__ == '__main__':
